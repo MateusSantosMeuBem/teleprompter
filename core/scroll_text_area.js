77cb7999f27dca995e8ca9ea4046dc552a499555
Mateus Santos <a.mateussm1.5@gmail.com>
2022-04-03 15:41:39 -0300

    text area scroll pt1

--- core/scroll_text_area.js
+++ core/scroll_text_area.js
@@ -1,6 +1,13 @@
 let scroll = false
 
-function scrollTextArea(){
+// const task = async () => {
+//     for (const item of list) {
+//       await new Promise(r => setTimeout(r, 1000));
+//       console.log('Yello, D\'oh');
+//     }
+//   }
+
+const scrollTextArea = async () => {
     console.log('Rolling...')
     let text_area = document.getElementById('text-area')
     let action = document.getElementById('action')
@@ -10,17 +17,21 @@ function scrollTextArea(){
     console.log(scroll)
     text_area.scrollTo(0, 0)
 
-    roof = 0
-    floor = text_area.scrollHeight - text_area.offsetHeight
+    // roof = 0
+    let floor = text_area.scrollHeight - text_area.offsetHeight
+    let point = 0
     if(scroll == true){
         text_area.scrollTo(0, 0)
-        while (roof != floor){
-            roof += 1
-            console.log('Waiting...')
-            setTimeout(function(){
-                text_area.scrollTo(0, roof)
-            }, 10000);
-            console.log('Go, go, go, go!')
+        for(let roof = 0; roof < floor; roof++){
+            // roof += 1
+            // console.log('Waiting...')
+            let speed =  document.getElementById('speed').value 
+            await new Promise(r => setTimeout(r, 50))
+            point = roof + speed
+            console.log(point)
+            text_area.scrollTo(0, point)
+            // console.log('Celeani ng interval...')
+            // clearInterval()
         }
     }
 }
